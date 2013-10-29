10e670757de0885daeaa8a6c0d01eb310a2aedf2
britzke <britzke@lette-verein.de>
2013-10-29 10:36:05 +0100

    Projekt eingerichtet und eine kleine Test-Datei erstellt

---
+++ testrepostory/src/main/java/testrepostory/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package testrepostory;
+
+/**
+ * @author britzke
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
